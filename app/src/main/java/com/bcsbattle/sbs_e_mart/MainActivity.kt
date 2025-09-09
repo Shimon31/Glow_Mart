ce5571cc1fc6a5464496e805d0f577134b788961
Shimon <shimonbhuiya02@gmail.com>
2025-09-09 19:40:19 +0600

    complete menu item

--- app/src/main/java/com/bcsbattle/sbs_e_mart/MainActivity.kt
+++ app/src/main/java/com/bcsbattle/sbs_e_mart/MainActivity.kt
@@ -5,18 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bcsbattle.sbs_e_mart.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Get reference to the BottomNavigationView
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        // Set up the listener for item selection
+
     }
 }
